0b96ad1e0f06d23a1b01cf880a214b36f8fab5f0
Sergio Oliveira <sergio@tracy.com.br>
2015-08-31 19:10:02 -0300

    Added setup.py
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+colab-gitlab plugin
+===================
+
+A Gitlab plugin for Colab.
+"""
+from setuptools import setup, find_packages
+
+install_requires = []
+
+tests_require = []
+
+
+setup(
+    name="colab-gitlab",
+    version='0.1.0',
+    author='Sergio Oliveira',
+    author_email='sergio@tracy.com.br',
+    url='https://github.com/colab/colab-gitlab-plugin',
+    description='A Gitlab plugin for Colab',
+    long_description=__doc__,
+    license='GPLv3',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
