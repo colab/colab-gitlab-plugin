fb2e2342d51552550c5b24f9e7eb9309afefe583
Sergio Oliveira <sergio@tracy.com.br>
2015-09-02 16:01:43 -0300

    Add COLAB_SETTINGS environment variable
    Signed-off-by: Sergio Oliveira <sergio@tracy.com.br>
    Signed-off-by: Lucas Kanashiro <kanashiro.duarte@gmail.com>
    Signed-off-by: Rafael Manzo <rr.manzo@gmail.com>
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,6 +9,7 @@ from django.test.utils import get_runner
 def run():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'colab.settings'
     os.environ['COLAB_PLUGINS'] = 'tests/plugins.d'
+    os.environ['COLAB_SETTINGS'] = 'tests/colab_settings.py'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
