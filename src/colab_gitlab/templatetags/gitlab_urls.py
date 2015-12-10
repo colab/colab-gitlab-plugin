56de28ad8142d01fc2070a4a5cdcf5d9bbf3cf57
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-12-10 12:06:45 -0200

    Fixed user links in search previews
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Lucas Kanashiro <kanashiro.duarte@gmail.com>

---
+++ src/colab_gitlab/templatetags/gitlab_urls.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag()
+def profile_url(user, user_url):
+    if not user:
+        return ""
+
+    html = '- <a href="{url}">{user}</a>'.format(url=user_url,
+                                                 user=user.title())
+    return html
