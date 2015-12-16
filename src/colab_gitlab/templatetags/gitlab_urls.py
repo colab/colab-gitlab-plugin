1db096d31944bd029d2596af9dac4321c6433c80
Matheus Faria <matheus.sousa.faria@gmail.com>
2015-12-16 11:00:20 -0200

    Fixing modified_by url
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Matheus Faria <matheus.sousa.faria@gmail.com>

--- src/colab_gitlab/templatetags/gitlab_urls.py
+++ src/colab_gitlab/templatetags/gitlab_urls.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag()
+@register.assignment_tag()
 def profile_url(user, user_url):
     if not user:
         return ""
