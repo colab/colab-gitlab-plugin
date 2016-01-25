58c6561494aa6ee1fcc50d7fcd24c19745f1f503
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-01-25 16:52:38 -0200

    Initial structure to login on gitlab
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_gitlab/tasks.py
+++ src/colab_gitlab/tasks.py
@@ -1,10 +1,28 @@
+from colab_gitlab.views import GitlabProxyView
 
-from colab.celery import app
+
+def authenticate_user(sender, user, request, **kwargs):
+    name = user.username
+    proxy_view = GitlabProxyView()
+    import ipdb; ipdb.set_trace()
+    gitlab_response = proxy_view.dispatch(request, 'profile')
+    print gitlab_response.status_code
+
+    location = gitlab_response.get('Location')
+    request.COOKIES.set('_remote_user', name)
+    request.META['HTTP_COOKIE'] += '; _remote_user={}'.format(name)
+    location = location.replace('/gitlab/', '')
+    gitlab_response = proxy_view.dispatch(request, location)
+
+    location = gitlab_response.get('Location')
+    session = gitlab_response.cookies.get('_gitlab_session').value
+    request.COOKIES.set('_gitlab_session', session)
+    request.META['HTTP_COOKIE'] += '; _gitlab_session={}'.format(session)
+
+    if gitlab_response.status_code == 200:
+        request.COOKIES.set('_gitlab_session',
+                            gitlab_response.cookies.get('_gitlab_session'))
 
 
-@app.task(bind=True)
-def handling_method(self, **kwargs):
-    f = open('/vagrant/test_plugin', 'wb')
-    f.write(str(kwargs))
-    f.close()
-    return 5
+def logout_user(sender, user, request, **kwargs):
+    request.COOKIES.delete('_noosfero_session')
