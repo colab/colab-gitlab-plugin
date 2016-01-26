1654a03a3713a811287e452e01e1cd903b87dab7
Luiz Oliveira <ziuloliveira@gmail.com>
2016-01-26 17:21:48 -0200

    login at gitlab when gitlab's user doesn't exist
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/colab_gitlab/tasks.py
+++ src/colab_gitlab/tasks.py
@@ -1,28 +1,36 @@
 from colab_gitlab.views import GitlabProxyView
+import re
+
+
+def get_location(response):
+    location = response.get('Location')
+    return re.sub('(.*)/gitlab/', '', location)
+
+
+def set_cookies(request, name, value):
+    request.COOKIES.set(name, value, path='/gitlab')
+    request.META['HTTP_COOKIE'] += '; {}={}'.format(name, value)
 
 
 def authenticate_user(sender, user, request, **kwargs):
-    name = user.username
+    request.method = 'GET'
     proxy_view = GitlabProxyView()
-    import ipdb; ipdb.set_trace()
     gitlab_response = proxy_view.dispatch(request, 'profile')
-    print gitlab_response.status_code
 
-    location = gitlab_response.get('Location')
-    request.COOKIES.set('_remote_user', name)
-    request.META['HTTP_COOKIE'] += '; _remote_user={}'.format(name)
-    location = location.replace('/gitlab/', '')
+    location = get_location(gitlab_response)
+    set_cookies(request, '_remote_user', user.username)
     gitlab_response = proxy_view.dispatch(request, location)
 
-    location = gitlab_response.get('Location')
+    location = get_location(gitlab_response)
     session = gitlab_response.cookies.get('_gitlab_session').value
-    request.COOKIES.set('_gitlab_session', session)
-    request.META['HTTP_COOKIE'] += '; _gitlab_session={}'.format(session)
+    set_cookies(request, '_gitlab_session', session)
+    gitlab_response = proxy_view.dispatch(request, location)
 
-    if gitlab_response.status_code == 200:
-        request.COOKIES.set('_gitlab_session',
-                            gitlab_response.cookies.get('_gitlab_session'))
+    session = gitlab_response.cookies.get('_gitlab_session').value
+    set_cookies(request, '_gitlab_session', session)
+
+    request.method = 'POST'
 
 
 def logout_user(sender, user, request, **kwargs):
-    request.COOKIES.delete('_noosfero_session')
+    request.COOKIES.delete('_gitlab_session', path='/gitlab')
