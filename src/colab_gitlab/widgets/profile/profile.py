0d31fe535faa59ef67a53eb5aef2dce14243b5d6
Macartur Sousa <macartur.sc@gmail.com>
2016-02-18 15:31:13 -0200

    Adding tests to GitlabProfileWidget
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>
    Signed-off-by: Matheus Faria <matheus.sousa.faria@gmail.com>

--- src/colab_gitlab/widgets/profile/profile.py
+++ src/colab_gitlab/widgets/profile/profile.py
@@ -1,8 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from colab_gitlab.views import GitlabProxyView, GitlabProfileProxyView
 from colab.widgets.widget_manager import Widget
-from django.utils.safestring import mark_safe
-from django.http import HttpRequest
 import re
 
 
@@ -11,19 +9,8 @@ class GitlabProfileWidget(GitlabProxyView, Widget):
     name = _('Development')
     default_url = '/gitlab/profile/account'
 
-    def get_body(self):
-        start = self.content.find('<body')
-        start = self.content.find('>', start)
-        end = self.content.find('</body>')
-
-        if -1 in [start, end]:
-            return ''
-
-        body = self.content[start + len('>'):end]
-        return mark_safe(body)
-
     def change_request_method(self, request):
-        if not len(request.POST) or request.POST.get('colab_form',None):
+        if not len(request.POST) or request.POST.get('colab_form', None):
             request.method = "GET"
         elif not request.POST.get("_method", None):
             request.method = "POST"
@@ -33,9 +20,9 @@ class GitlabProfileWidget(GitlabProxyView, Widget):
     def generate_content(self, **kwargs):
         request = kwargs.get('context', {}).get('request', None)
         is_colab_form = request.POST.get('colab_form', False)
-        path = request.GET.get('path',False)
+        path = request.GET.get('path', '')
 
-        if  is_colab_form or not path :
+        if is_colab_form or not path:
             requested_url = self.default_url
         else:
             requested_url = path
@@ -43,26 +30,25 @@ class GitlabProfileWidget(GitlabProxyView, Widget):
         self.change_request_method(request)
         requested_url = self.fix_requested_url(requested_url)
 
-        g = GitlabProfileProxyView()
-        request = self.login_in_gitlab(request,g)
-        response =  g.dispatch(request, requested_url)
+        gitlab_proxy_view = GitlabProfileProxyView()
+        self.login_in_gitlab(request)
+        response = gitlab_proxy_view.dispatch(request, requested_url)
 
         if response.status_code == 302:
             requested_url = self.fix_requested_url(response.get('Location'))
             request.method = 'GET'
-            response =  g.dispatch(request, requested_url)
+            response = gitlab_proxy_view.dispatch(request, requested_url)
 
-        if hasattr(response,'content'):
+        if hasattr(response, 'content'):
             self.content = response.content
         else:
             self.content = "".join(response.streaming_content)
 
-    def fix_requested_url(self,url):
+    def fix_requested_url(self, url):
         return re.sub('^(.*)/gitlab/', '', url)
 
-    def login_in_gitlab(self,request,g):
+    def login_in_gitlab(self, request):
         token = '32e80744e104cd10c909289694ea1164'
         request.COOKIES['_remote_user'] = 'macartur'
         request.COOKIES['_gitlab_session'] = token
         request.META['HTTP_COOKIE'] += '; _remote_user=macartur; _gitlab_session='+token
-        return request
---
+++ tests/test_widgets.py
@@ -0,0 +1,110 @@
+from mock import patch
+
+from django.test import TestCase
+from django.http import HttpRequest, HttpResponse, StreamingHttpResponse
+from colab_gitlab.widgets.profile.profile import GitlabProfileWidget
+from colab_gitlab.views import GitlabProfileProxyView
+
+
+class WidgetsTest(TestCase):
+
+    module_path = 'colab_gitlab.widgets.profile.profile.GitlabProfileWidget'
+
+    def setUp(self):
+        self.profile_widget = GitlabProfileWidget()
+        self.current_request = HttpRequest()
+        self.current_request.method = 'POST'
+
+        self.http_response = HttpResponse()
+        self.streaming_http_response = StreamingHttpResponse()
+
+    def test_change_request_method_with_colab_form(self):
+        self.current_request.POST = {'colab_form': 'true'}
+        self.profile_widget.change_request_method(self.current_request)
+
+        self.assertEquals(self.current_request.method, 'GET')
+
+    def test_change_request_method_with_GET_request(self):
+        self.current_request.POST = {}
+        self.current_request.method = 'GET'
+        self.profile_widget.change_request_method(self.current_request)
+
+        self.assertEquals(self.current_request.method, 'GET')
+
+    def test_change_request_method_with_post_and_no__method(self):
+        self.current_request.POST = {'user': 'sample_user'}
+        self.profile_widget.change_request_method(self.current_request)
+
+        self.assertEquals(self.current_request.method, 'POST')
+
+    def test_change_request_method_with_post_and__method(self):
+        self.current_request.POST = {'_method': 'sample_method',
+                                     'user': 'sample_user'}
+        self.profile_widget.change_request_method(self.current_request)
+
+        self.assertEquals(self.current_request.method, 'SAMPLE_METHOD')
+
+    def test_fix_requested_url(self):
+        url = 'http://localhost:8000/gitlab/account/test'
+        self.assertEquals(self.profile_widget.fix_requested_url(url),
+                          'account/test')
+
+        url = 'http://localhost:8000/account/test'
+        self.assertEquals(self.profile_widget.fix_requested_url(url),
+                          url)
+
+    @patch(module_path + '.fix_requested_url')
+    @patch.object(GitlabProfileProxyView, 'dispatch')
+    def test_generate_content(
+            self, dispatch_mock, fix_requested_url_mock):
+        fix_requested_url_mock.return_value = 'test'
+
+        self.http_response.status_code = 302
+        self.http_response['Location'] = 'test/url'
+
+        content = '<head></head><body></body>'
+        self.http_response.content = content
+
+        dispatch_mock.return_value = self.http_response
+
+        self.current_request.POST = {'colab_form': True}
+        self.current_request.GET = {'path': '/gitlab/test'}
+        self.current_request.META['HTTP_COOKIE'] = ''
+        params = {'context': {'request': self.current_request}}
+
+        self.profile_widget.generate_content(**params)
+        self.assertEquals(content, self.profile_widget.content)
+        self.assertEquals(2, dispatch_mock.call_count)
+
+        self.current_request.POST = {}
+        self.current_request.GET = {}
+        self.profile_widget.generate_content(**params)
+        self.assertEquals(content, self.profile_widget.content)
+
+        self.current_request.GET = {'path': '/gitlab/test'}
+        self.profile_widget.generate_content(**params)
+        self.assertEquals(content, self.profile_widget.content)
+
+        self.http_response.status_code = 200
+        self.current_request.POST = {'colab_form': True}
+        self.profile_widget.generate_content(**params)
+        self.assertEquals(content, self.profile_widget.content)
+
+    @patch(module_path + '.fix_requested_url')
+    @patch.object(GitlabProfileProxyView, 'dispatch')
+    def test_generate_content_using_streaming_content(
+            self, dispatch_mock, fix_requested_url_mock):
+
+        fix_requested_url_mock.return_value = 'test'
+        self.streaming_http_response.status_code = 200
+        dispatch_mock.return_value = self.streaming_http_response
+
+        streaming_content = ["sample ", "streaming ", "string ", "content."]
+        self.streaming_http_response.streaming_content = streaming_content
+        self.current_request.META['HTTP_COOKIE'] = ''
+
+        params = {'context': {'request': self.current_request}}
+        self.profile_widget.generate_content(**params)
+
+        content = ''.join(streaming_content)
+        self.assertEquals(content, self.profile_widget.content)
