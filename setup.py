0da236d0286534e2a3d9a654639936266300d299
Macartur Sousa <macartur.sc@gmail.com>
2016-06-06 10:57:41 -0300

    Bump version 0.2.14 ==> 0.2.15
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
 
 setup(
     name="colab-gitlab",
-    version='0.2.14',
+    version='0.2.15',
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     url='https://github.com/colab/colab-gitlab-plugin',
